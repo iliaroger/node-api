ae1eeb6e0930e91bde593089e27a9e8b69276681
ilia <iliailia@me.com>
2020-07-18 21:44:23 +0200

    reinstalled packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MainView from './components/mainView';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Placeholder API</h3>
+    <div className="container mainContainer">
+      <MainView></MainView>
     </div>
   );
 }
---
+++ src/components/mainView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function mainView() {
+  return (
+    <>
+      <div className="row mainViewRow">
+        <div className="col-md-12">
+          <h1>Placeholder API</h1>
+        </div>
+      </div>
+    </>
+  );
+}
