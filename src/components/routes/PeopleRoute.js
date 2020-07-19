7c9d6c7bf670a0ae95363ae0f6bc992fb82cf75e
ilia <iliailia@me.com>
2020-07-19 20:54:43 +0200

    added a button

--- src/components/routes/PeopleRoute.js
+++ src/components/routes/PeopleRoute.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 export default function PeopleRoute() {
   const [peopleData, setPeopleData] = useState({});
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -19,6 +20,11 @@ export default function PeopleRoute() {
         <div className="col-md-12">
           <h3>People data in JSON format</h3>
           <pre>{dataLoaded ? peopleData : null}</pre>
+        </div>
+        <div className="col-md-12">
+          <Link to="/">
+            <Button>Go Back</Button>
+          </Link>
         </div>
       </div>
     </div>
