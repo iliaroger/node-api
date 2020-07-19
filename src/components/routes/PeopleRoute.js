7eb91723dd566c68e62a835277521cc0966edbab
ilia <iliailia@me.com>
2020-07-19 20:16:02 +0200

    minor changes to properly display json

--- src/components/routes/PeopleRoute.js
+++ src/components/routes/PeopleRoute.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 
 export default function PeopleRoute() {
   const [peopleData, setPeopleData] = useState({});
+  const [dataLoaded, setDataLoaded] = useState(false);
 
   useEffect(() => {
     fetch('/people')
       .then((res) => res.json())
-      .then((json) => setPeopleData(json));
+      .then((json) => {
+        setPeopleData(JSON.stringify(json, undefined, 2));
+        setDataLoaded(true);
+      });
   }, []);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <p>{console.log(peopleData)}</p>
+          <h3>People data in JSON format</h3>
+          <pre>{dataLoaded ? peopleData : null}</pre>
         </div>
       </div>
     </div>
